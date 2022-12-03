a59a88c6c9959d053dcde83f68f9692ee071d32d
Amauri <hamauri@aol.com>
2022-12-02 17:18:04 -0800

    added seeds

---
+++ db/query.sql
@@ -0,0 +1,11 @@
+-- SELECT a.first_name AS employee, b.first_name AS Manager
+-- FROM employee a, employee b 
+-- WHERE a.id = b.manager_id;
+
+SELECT a.id,
+    CONCAT(a.first_name, " ", a.last_name) AS employee, 
+    role.title AS role,
+    CONCAT(b.first_name, " ", b.last_name) AS Manager
+FROM employee a
+LEFT JOIN employee b ON a.manager_id = b.id
+LEFT JOIN role ON a.role_id = role.id;
