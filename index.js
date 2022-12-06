70de349563b5cac47915e63804c06fdf2d973399
Amauri <hamauri@aol.com>
2022-12-06 10:13:33 -0800

    emv added

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
 const mysql = require('mysql2');
-const inquirer = require('inquirer');
+const { prompt } = require('inquirer');
 const cTable = require('console.table');
 const questions = require('./lib/questions');
 
 // create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'password0123',
-    database: 'employees_db'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 const onLoad = () => {
     mainMenu();
 }
 
-const mainMenu = () => {
-    inquirer
-        .prompt(questions.getMenu())
+const mainMenu = async () => {
+    // const answers = await prompt(questions);
+    prompt(questions.getMenu())
+        // export to function
         .then(answers => {
             const choice = answers.menuChoice;
 
@@ -48,7 +50,7 @@ const mainMenu = () => {
 
 const viewTable = table => {
     const query = `SELECT * FROM ${table}`
-
+    // pass in connection through function
     connection.query(query, (err, results) => {
         if (err) {
             console.error(err);
@@ -61,7 +63,7 @@ const viewTable = table => {
 }
 
 const addDepartment = () => {
-    inquirer.prompt(questions.newDepartment()).then(answers => {
+    prompt(questions.newDepartment()).then(answers => {
         const { departmentName } = answers;
         const query = 'INSERT INTO department (name) VALUES (?)'
 
@@ -82,13 +84,12 @@ const addRole = table => {
         if (err) {
             console.error(err);
         }
-        
+
         const choices = results.map(obj => {
             return { name: obj.name, value: obj.id }
         })
 
-        inquirer
-            .prompt(questions.newRole(choices))
+        prompt(questions.newRole(choices))
             .then(answers => {
                 const query = `INSERT INTO ${table} (title, salary, department_id) VALUES (?, ?, ?)`;
                 const values = [answers.title, answers.salary, answers.department_id]
