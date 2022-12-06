2d551d095964a3c70d99b77126f51986d6c52e4d
Amauri <hamauri@aol.com>
2022-12-06 12:08:14 -0800

    added comments to code

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 require('dotenv').config();
-const getConnection = require('./config/connection')
+const getConnection = require('./config/connection');
 const { prompt } = require('inquirer');
 const cTable = require('console.table');
 const questions = require('./lib/questions');
 
+// sets up mysql connection
 const connection = getConnection();
 
 const init = async () => {
     mainMenu();
 }
 
+// loads main menu
 const mainMenu = async () => {
-    const answers = await prompt(questions.getMenu())
+    const answers = await prompt(questions.getMenu());
     navigation(answers.menuChoice);
 }
 
+// calls specific function depending on department selected
 const navigation = async menuChoice => {
     switch (menuChoice) {
         case 'department':
@@ -42,50 +45,57 @@ const navigation = async menuChoice => {
     mainMenu();
 }
 
+// loads table depending on table name passed in
 const viewTable = async table => {
-    const query = `SELECT * FROM ${table}`
+    const query = `SELECT * FROM ${table}`;
     // pass in connection through function
     const [results, buffer] = await (await connection).execute(query);
     console.table('', results);
 }
 
+// adds new department to department table
 const addDepartment = async () => {
-    const answers = await prompt(questions.newDepartment())
+    const answers = await prompt(questions.newDepartment());
     const { departmentName } = answers;
-    const query = 'INSERT INTO department (name) VALUES (?)'
+    const query = 'INSERT INTO department (name) VALUES (?)';
 
     await (await connection).execute(query, [departmentName]);
 
     console.log(`The ${departmentName} department has been added\n`);
 }
 
+// adds new role to role table
 const addRole = async table => {
+    //saves all department data
     const [departments, buffer] =
-        await (await connection).execute('SELECT * FROM department')
+        await (await connection).execute('SELECT * FROM department');
 
+    // changes 'id' to 'value' to be used in inquirer format
     const choices = departments.map(obj => ({ name: obj.name, value: obj.id }));
-    const answers = await prompt(questions.newRole(choices))
+    const answers = await prompt(questions.newRole(choices));
 
     const query = `INSERT INTO ${table} (title, salary, department_id) VALUES (?, ?, ?)`;
-    const values = [answers.title, answers.salary, answers.department_id]
+    const values = [answers.title, answers.salary, answers.department_id];
 
-    const [results, buffer2] = await (await connection).execute(query, values)
+    await (await connection).execute(query, values);
 
     console.log(`The ${values[0]} role has been added\n`);
 }
 
+// adds new employee to employee table
 const addEmployee = async () => {
-    const query = ''
+    const query = '';
 
-    const [results, buffer] = await (await connection).execute(query)
+    const [results, buffer] = await (await connection).execute(query);
     console.table('', results);
 }
 
+// updates employee role
 const updateEmployeeRole = async () => {
-    const query = ''
+    const query = '';
 
     const [results, buffer] =
-        await (await connection).execute(query)
+        await (await connection).execute(query);
 
     console.table('', results);
 }
